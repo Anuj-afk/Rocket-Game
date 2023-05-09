5adaaff196ed85003cf428002f9b92a58ebd5947
Anuj_Gupta6263 <anujg6263@gmail.com>
2023-05-10 00:41:14 +0530

    added a way to put the game in a loop

--- Assets/codes/ColllisionHandler.cs
+++ Assets/codes/ColllisionHandler.cs
@@ -16,10 +16,11 @@ public class ColllisionHandler : MonoBehaviour
                 Debug.Log("This is freindly");
                 break;
             case "Finish":
-                level += 1;
+                level = 1;
                 LevelLoader();
                 break;
             default:
+                level = 0;
                 LevelLoader();
                 break;
         }
@@ -27,6 +28,13 @@ public class ColllisionHandler : MonoBehaviour
     private void LevelLoader()
     {
         int Currentscene = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(Currentscene + level);
+        if (Currentscene + level == SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(Currentscene + level);
+        }
     }
 }
